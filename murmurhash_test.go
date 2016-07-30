8d035862fc3eec9797dc0a35929e818566c79d4f
mausimag <mmmauriciosilvam@gmail.com>
2016-07-29 23:45:11 -0300

    Fiz hash

--- murmurhash_test.go
+++ murmurhash_test.go
@@ -6,7 +6,9 @@ import (
 )
 
 func TestMurmurhash(t *testing.T) {
-	b := []byte("test")
-	v := Murmurhash3_x86_32(b, len(b), 0)
-	log.Printf(">%d", v)
+	b := []byte("teste1")
+	log.Printf(">%d", Murmurhash3_x86_32(b, len(b), 0))
+
+	b2 := []byte("teste2")
+	log.Printf(">%d", Murmurhash3_x86_32(b2, len(b2), 0))
 }
